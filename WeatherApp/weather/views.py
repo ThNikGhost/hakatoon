29b4f42ee49898990e9de6363a51fc824bc580af
ThGhost <orlov.icloud@mail.ru>
2023-02-22 22:24:43 +0600

    обновил данные по видео

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,3 +1,12 @@
+import requests
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    appid = '3fd71f50684a58f9c310b7a4e5df243f'
+    city = 'London'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={appid}'
+
+    
+    res = requests.get(url)
+    print(res.text)
+    return render(request, 'weather/index.html')
