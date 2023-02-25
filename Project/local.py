c4d3bcc6efa0d8474f8daaccee2fc7f1ee2d8f27
ThGhost <orlov.icloud@mail.ru>
2023-02-25 22:06:32 +0600

    Добавил возможность сохранения данных в БД

--- Project/local.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-
-res = requests.get(f"http://127.0.0.1:3000/api/main")
-print(res.json())
-
--- Project/main.py
+++ Project/main.py
@@ -11,19 +11,18 @@ url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=3
 class Main(Resource):
     def get(self, city):
         res = requests.get(url.format(city)).json()
-        try:
-            city_info = {
-                'city': res['name'],                       # Название города 
-                'temp': res['main']['temp'],               # Температура в городе
-                'temp_feels': res['main']['feels_like'],   # Температура по ощущениям
-                'icon': res['weather'][0]['icon'],        # Скорость ветра (вроде м/c)
+        city_info = {
+                'city': res['name'],
+                'temp': res['main']['temp'],
+                'temp_feels': res['main']['feels_like'],
+                'icon': res['weather'][0]['icon'],
             }
-        except KeyError:
-            return 'Incorect name'    #Если неверное имя города, то возвращает эту строку 
         return city_info
 
-class City(Resource):
-    def get(self, lang):
+
+class WorkDataBase(Resource):
+    def get(self, text):
+        lang = text
         with sq.connect('City.sqlite3') as con:
             cur = con.cursor()
             cur.execute(f'''SELECT {lang}_name
@@ -33,6 +32,39 @@ class City(Resource):
             json_string = json.dumps(data_list,ensure_ascii = False)
             response = Response(json_string,content_type="application/json; charset=utf-8" )
         return response
+    
+    def post(self, text):
+        name_city = text
+        
+        if text == '': # Проверка пустая ли строка, если да, то просто возвращает все данные
+            with sq.connect('City.sqlite3') as con:
+                cur = con.cursor()
+                cur.execute('SELECT Name FROM City_save')
+                return cur.fetchall()
+
+        with sq.connect('City.sqlite3') as con:
+            cur = con.cursor()
+            cur.execute('SELECT Name FROM City_save')
+            data = get_list_db(cur.fetchall())
+
+            if name_city in data: # Проверяет, есть ли введённая строка уже в БД
+                return data
+            
+            cur.execute('SELECT rowid FROM City_save')
+            data_new = get_list_db(cur.fetchall())
+
+            if len(data) <= 2:
+                cur.execute(f"""INSERT INTO City_save (Name) VALUES ('{name_city}')""")
+                con.commit()
+            elif len(data) > 2:
+                cur.execute(f"DELETE FROM City_save WHERE rowid = '{data_new[0]}';")
+                con.commit()
+                cur.execute(f"""INSERT INTO City_save (Name) VALUES ('{name_city}')""")
+                con.commit()
+            
+            cur.execute(f"""SELECT Name FROM City_save""")
+            data_list = get_list_db(cur.fetchall())
+            return data_list
 
 
 
@@ -50,7 +82,7 @@ def index():
 
 
 api.add_resource(Main, '/api/main/<string:city>')
-api.add_resource(City, '/api/cities/<string:lang>')
+api.add_resource(WorkDataBase, '/api/cities/<string:text>')
 app.config['JSON_AS_UTF-8'] = False
 api.init_app(app)
 
